df08a1a4f2ea169646950cc6ba14db9cccc874cb
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-28 20:55:31 -0300

    separando rotas da api

---
+++ api-rest/src/routes/products.ts
@@ -0,0 +1,11 @@
+import { ProductsController } from "@/controllers/products-controller";
+import { myMiddleware } from "@/middlewares/my-middlewares";
+import { Router } from "express";
+
+const productsRoutes = Router();
+const productsControllers = new ProductsController()
+
+productsRoutes.get("/", productsControllers.index);
+productsRoutes.post("/", myMiddleware, productsControllers.create);
+
+export { productsRoutes };
