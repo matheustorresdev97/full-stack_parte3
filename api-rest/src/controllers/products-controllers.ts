1e6da2fcc5e5a66fb2269990a5644704a658c91b
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-28 21:10:26 -0300

    projeto finalizado

--- api-rest/src/controllers/products-controllers.ts
+++ api-rest/src/controllers/products-controllers.ts
@@ -1,18 +1,24 @@
 import { Request, Response } from "express";
-
+import { z } from "zod";
 
 export class ProductsController {
-    index(request: Request, response: Response) {
-        const { page, limit } = request.query;
+  index(request: Request, response: Response) {
+    const { page, limit } = request.query;
 
-        response.send(`Página ${page} de ${limit}`);
-    }
+    response.send(`Página ${page} de ${limit}`);
+  }
 
-    create(request: Request, response: Response) {
+  create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z
+        .string({ required_error: "Name is required!" })
+        .trim()
+        .min(6, { message: "Name Must be 6 or more characters" }),
+      price: z.number({ required_error: "Price is required!" }).positive({ message: "Price must be positive" }).gte(10),
+    });
 
+    const { name, price } = bodySchema.parse(request.body);
 
-        const { name, price } = request.body;
-
-        response.status(201).json({ name, price, user_id: request.user_id });
-    }
+    response.status(201).json({ name, price, user_id: request.user_id });
+  }
 }
--- api-rest/src/server.ts
+++ api-rest/src/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 import { routes } from "./routes";
-
+import { AppError } from "./utils/AppError";
+import { ZodError } from "zod";
 
 const PORT = 3333;
 
@@ -9,5 +10,25 @@ app.use(express.json());
 
 app.use(routes);
 
+const errorHandler: ErrorRequestHandler = (
+  error: Error | AppError,
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void => {
+    if(error instanceof AppError) {
+        response.status(error.statusCode).json({message: error.message});
+        return;
+    }
+
+    if(error instanceof ZodError) {
+        response.status(400).json({ message: "Validation error!", issues: error.format() });
+        return;
+    }
+
+    response.status(500).json({message: error.message});
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
